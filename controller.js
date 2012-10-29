63a6f69c6b1f79273d104dce7c8ef7b991dfff52
Bryan Knowles <snotskie@yahoo.com>
2012-10-29 11:55:14 -0500

    Display interface in place

--- controller.js
+++ controller.js
@@ -1,4 +1,22 @@
+function Piece(color, i, j){
+	var self = {};
+	self.color = color;
+	self.i = i;
+	self.j = j;
+	return self;
+}
+
 function BlobsCtrl($scope){
+	$scope.board = [];
+	for (var i = 0; i < 7; ++i){
+		$scope.board.push([]);
+		for (var j = 0; j < 7; ++j){
+			$scope.board[i].push(Piece("", i, j));
+		}
+	}
+
+	$scope.board[0][0].color = $scope.board[6][6].color = "black";
+	$scope.board[0][6].color = $scope.board[6][0].color = "white";
 	$scope.shitbrix = function(msg){
 		console.log(msg)
 	};
