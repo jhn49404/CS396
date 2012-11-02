d73641de6433f4f539c9e460ae165d1289ce7e51
Bryan Knowles <snotskie@yahoo.com>
2012-11-02 12:11:31 -0500

    Adding RandomAI stuff

--- computers.js
+++ computers.js
@@ -1,3 +1,12 @@
+// NOTE: initialize WITH "new" keyword
+function State(board){
+	// this.asdfasrgwerg = asdgasfdgasg
+
+	// NOTE: changes stores the changes made to the board
+	// e.g., changes[0] = [{i:0, j:0, color:"white"}, ...]
+	this.changes = [];
+}
+
 function isTerminal(state){
 	// TODO
 }
@@ -8,14 +17,30 @@ function overChildren(state, cb){
 	// remember to use palindromic pruning to not send two equivalent children
 }
 
+// NOTE: initialize WITHOUT "new" keyword
 function RandomAI(color, board){
 	var self = {};
 	self.color = color;
 	self.board = board;
 	self.choose = function(cb){
+
 		// NOTE: using setTimeout to implement concurrency
 		setTimeout(function(){
-			// ...
+			var root = new State(board);
+			var most = -Infinity;
+			var choice = null;
+
+			// NOTE: assigning each child a random value,
+			// keeping only the highest assigned.
+			overChildren(root, function(child){
+				var X = Math.random();
+				if (X > most){
+					most = X;
+					choice = child;
+				}
+			});
+
+			cb(choice.changes[0]);
 		}, 50);
 	};
 
@@ -35,7 +60,7 @@ function minimax(state, ply){
 	} else {
 		alpha = -Infinity;
 
-		// NOTE: use callback form to simulate python's yield keyword
+		// NOTE: using callback form to simulate python's yield keyword
 		overChildren(state, function(child){
 			alpha = max(alpha, -minimax(child, ply-1));
 		});
@@ -44,6 +69,7 @@ function minimax(state, ply){
 	}
 }
 
+// NOTE: initialize WITHOUT "new" keyword
 function MinimaxAI(color, board){
 	var self = {};
 	self.board = board;
@@ -58,3 +84,13 @@ function MinimaxAI(color, board){
 	// ...
 	return self;
 }
+
+// NOTE: State is initialized with the "new" keyword,
+// whereas the AI classes are not. This is because,
+// in Javascript, objects initialized using the "new"
+// method are initialized faster, whereas those initialized
+// with the "self" method have their properties looked up faster.
+// Because we are making a LOT of states in overChildren,
+// we want them to be initialized as quickly as possible.
+// Because we only initialize the AI once (when BlobCtrl loads),
+// we use the "self" method for them.
